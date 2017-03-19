d33139d032be18edb7579412d72971087aba6eed
Niemetz <nguyen7744@yahoo.com>
2017-03-19 11:59:38 -0400

    Commited

--- src/test/java/auto/pages/HomePage.java
+++ src/test/java/auto/pages/HomePage.java
@@ -13,7 +13,7 @@ public class HomePage extends AmazonBasePageObject
 			mapTable.put("Sign in securely".toLowerCase(),"#a-autoid-0-announce");
 			mapTable.put("Account & Lists".toLowerCase(),".//*[@id='nav-link-accountList']/span[2]");
 			mapTable.put("Page Unique Element".toLowerCase(), "#a-autoid-0-announce");	
-			// Update 1 by user 2 to Remote Development Branch
+			// Update #2 by user 1 to Remote Development Branch.
 		}
 }
 
