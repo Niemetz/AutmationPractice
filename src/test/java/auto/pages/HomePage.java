64c2e90d7f6c8f17766c9839e93ebb432bf836c1
Niemetz <nguyen7744@yahoo.com>
2017-03-19 02:49:41 -0400

    Update HomePage.java

--- src/test/java/auto/pages/HomePage.java
+++ src/test/java/auto/pages/HomePage.java
@@ -13,7 +13,7 @@ public class HomePage extends AmazonBasePageObject
 			mapTable.put("Sign in securely".toLowerCase(),"#a-autoid-0-announce");
 			mapTable.put("Account & Lists".toLowerCase(),".//*[@id='nav-link-accountList']/span[2]");
 			mapTable.put("Page Unique Element".toLowerCase(), "#a-autoid-0-announce");	
-			// Update 13 from  user 1 to RemoteDevelopment Branch
+			// Update 14 from  user 2 to RemoteDevelopment Branch
 		}
 }
 
