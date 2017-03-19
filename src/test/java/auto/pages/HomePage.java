3db9eb418b540a32b289cb3123efd19d2240a2ea
Niemetz <nguyen7744@yahoo.com>
2017-03-19 02:47:52 -0400

    Update 13 from  user 1 to RemoteDevelopment Branch

--- src/test/java/auto/pages/HomePage.java
+++ src/test/java/auto/pages/HomePage.java
@@ -13,7 +13,7 @@ public class HomePage extends AmazonBasePageObject
 			mapTable.put("Sign in securely".toLowerCase(),"#a-autoid-0-announce");
 			mapTable.put("Account & Lists".toLowerCase(),".//*[@id='nav-link-accountList']/span[2]");
 			mapTable.put("Page Unique Element".toLowerCase(), "#a-autoid-0-announce");	
-			// Update 12 from  user 1 to RemoteDevelopment Branch
+			// Update 13 from  user 1 to RemoteDevelopment Branch
 		}
 }
 
