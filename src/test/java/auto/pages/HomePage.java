8d0bd6618221fea7ab394c3b6155509e3d22b809
Niemetz <nguyen7744@yahoo.com>
2017-03-22 18:29:14 -0400

    Update #4 by John to Remote Master Branch...

--- src/test/java/auto/pages/HomePage.java
+++ src/test/java/auto/pages/HomePage.java
@@ -13,7 +13,7 @@ public class HomePage extends AmazonBasePageObject
 			mapTable.put("Sign in securely".toLowerCase(),"#a-autoid-0-announce");
 			mapTable.put("Account & Lists".toLowerCase(),".//*[@id='nav-link-accountList']/span[2]");
 			mapTable.put("Page Unique Element".toLowerCase(), "#a-autoid-0-announce");	
-			// Update #3 by John to Remote Development Branch...
+			// Update #4 by John to Remote Master Branch...
 		}
 }
 
