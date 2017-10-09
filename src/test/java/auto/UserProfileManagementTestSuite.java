6952be25faf98de5278418bb4a3fc38532306a98
Niemetz <nguyen7744@yahoo.com>
2017-10-09 19:11:57 -0400

    Update the pom file and the serenity file

---
+++ src/test/java/auto/UserProfileManagementTestSuite.java
@@ -0,0 +1,16 @@
+package auto;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions( 
+		//features={"src/test/resources/features/admin/user_management/user_Login_authentication"}
+		features={"src/test/resources/features/admin/user_profile_management/view_order_history_and_details"}
+		//,tags={" @Creation-Of-Type-Analyst-1"}
+		)
+
+public class UserProfileManagementTestSuite {
+
+}
