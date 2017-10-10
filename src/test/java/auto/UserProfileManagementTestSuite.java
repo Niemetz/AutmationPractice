14cb212666f12874469a0a6afa8ddd0cc9f1e802
Niemetz <nguyen7744@yahoo.com>
2017-10-09 20:24:16 -0400

    Updated the narrative files

--- src/test/java/auto/UserProfileManagementTestSuite.java
+++ src/test/java/auto/UserProfileManagementTestSuite.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions( 
-		//features={"src/test/resources/features/admin/user_management/user_Login_authentication"}
 		features={"src/test/resources/features/admin/user_profile_management/view_order_history_and_details"}
 		//,tags={" @Creation-Of-Type-Analyst-1"}
 		)
