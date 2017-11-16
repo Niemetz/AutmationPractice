8e75dd625198df5e4d8eeb669b01e54fba64c6fe
Niemetz <nguyen7744@yahoo.com>
2017-11-16 09:54:32 -0500

    try 2

---
+++ src/test/java/auto/UserProfileManagementTestSuite.java
@@ -0,0 +1,16 @@
+package auto;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions( 
+		features={"src/test/resources/features/admin/user_management/user_Login_authentication"}
+		,plugin = {"html:target/MyTest"}
+		,tags={"@History-And-Details"}
+		)
+public class UserProfileManagementTestSuite {
+
+}
