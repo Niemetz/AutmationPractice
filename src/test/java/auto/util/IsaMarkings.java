90d630d7fbde870b70b62bfe2194f36f9644555b
Niemetz <nguyen7744@yahoo.com>
2018-12-19 22:59:34 -0500

    Make change to these files

--- src/test/java/auto/util/IsaMarkings.java
+++ src/test/java/auto/util/IsaMarkings.java
@@ -5,33 +5,40 @@ import java.util.Map;
 
 public class IsaMarkings {
 	String pageID;
+
 	public String getPageID() {
 		return pageID;
 	}
+
 	public void setPageID(String pageID) {
 		this.pageID = pageID;
 	}
+
 	///////////////////////////
-	String sectionID ;
+	String sectionID;
+
 	public String getSectionID() {
 		return sectionID;
 	}
+
 	public void setSectionID(String sectionID) {
 		this.sectionID = sectionID;
 	}
-    ///////////////////////////
-    Map<String, String> mainSection = new HashMap<>();
+
+	///////////////////////////
+	Map<String, String> mainSection = new HashMap<>();
 
 	public Map<String, String> isaMarkingsMain() {
 		return mainSection;
 	}
+
 	public void setMainSection(Map<String, String> mainSection) {
 		this.mainSection = mainSection;
 	}
-	public IsaMarkings()
-	{
+
+	public IsaMarkings() {
 		this.pageID = null;
 		this.sectionID = null;
-        this.mainSection = null;
-	}	
+		this.mainSection = null;
+	}
 }
